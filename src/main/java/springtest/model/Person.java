480d958c455cd983ed829cf24b2a78df6a062e42
pavul zavala <pavulzavala@gmail.com>
2018-02-12 20:34:34 -0600

    adding project lombok to repository

--- src/main/java/springtest/model/Person.java
+++ src/main/java/springtest/model/Person.java
@@ -5,32 +5,35 @@
  */
 package springtest.model;
 
+import lombok.Data;
+
 /**
  *
  * @author pavulzavala
  */
+@Data
 public class Person 
 {
    
     private String name;
     private String lastname;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getLastname() {
-        return lastname;
-    }
-
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
-    }
-    
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getLastname() {
+//        return lastname;
+//    }
+//
+//    public void setLastname(String lastname) {
+//        this.lastname = lastname;
+//    }
+//    
     
     
 }//
